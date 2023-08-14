32ce76607eb1628e73dfe2d3d806f160a9d74342
Mindaugas Bernotas <mindaugas@bernotas.lt>
2023-08-15 01:19:05 +0300

    Rewrite README and install script

--- README.md
+++ README.md
@@ -4,45 +4,11 @@ My personal dotfiles, so far including `zsh` and `neovim`.
 
 # Installation
 
-Clone and run
-```sh
-./install.sh
-# or
-./install.sh --symbolic
-```
-
-Change shell
-```sh
-chsh -s /bin/zsh
-```
-
-## [Vim-plug](https://github.com/junegunn/vim-plug)
-
-Install for `neovim`:
+Ensure `git` is installed and run the following to clone the repository in a
+temporary directory and run `install.sh` script:
 
 ```sh
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-```
-
-## Packages
-
-[neovim (latest)](https://github.com/neovim/neovim/releases/latest) |
-[zsh-completions](https://software.opensuse.org/download.html?project=shells:zsh-users:zsh-completions&package=zsh-completions)
-
-### Debian
-
-```sh
-apt install -y git neovim zsh zsh-autosuggestions zsh-syntax-highlighting
-```
-
-### Fedora
-
-```sh
-dnf install -y git neovim zsh zsh-autosuggestions zsh-syntax-highlighting
-```
-
-### Arch
-
-```sh
-pacman -S --noconfirm git neovim zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+tmpdir="$(mktemp -d)"
+git clone 'https://github.com/sewiti/dotfiles.git' "$tmpdir" && "$tmpdir/install.sh"
+rm -rf "$tmpdir"
 ```
--- install.sh
+++ install.sh
@@ -1,51 +1,22 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -xe
 
-# Packages
-if [ -f /etc/debian_version ]; then
-	apt update
-	apt install --yes curl git jq neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting
-elif [ -f /etc/redhat-release ]; then
-	yum install --assumeyes curl git jq neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting
-elif [ -f /etc/arch-release ]; then
-	pacman -Sy --noconfirm curl git jq neovim wget zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
-elif [ -f /etc/alpine-release ]; then
-	apk update
-	apk add curl git jq neovim wget zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+if   [[ "${OSTYPE}" == darwin* ]]; then echo "TODO"
+elif command -v apt    >/dev/null; then apt update && apt install -y        curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting passwd
+elif command -v dnf    >/dev/null; then dnf install -y                      curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting util-linux-user
+elif command -v zypper >/dev/null; then zypper refresh && zypper install -y curl git neovim wget zsh shadow 
+elif command -v pacman >/dev/null; then pacman -Sy --noconfirm              curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting util-linux zsh-completions
 fi
 
-# Neovim plugins
-curl -fLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-nvim --headless +PlugInstall +qa
+srcdir="$(dirname "$0")"
+cp -f  "${srcdir}/.zshenv" "${HOME}/.zshenv"
+cp -fa "${srcdir}/.config" "${HOME}/.config"
+mkdir -p "${HOME}/.cache/zsh"
 
-# Options
-TARGET="${TARGET:-$HOME}"
-for arg in "$@"; do
-    case "$arg" in
-        -s|-symbolic|--symbolic)
-            USE_SYMLINKS=1
-            ;;
-        *)
-            echo "unknown argument: ${arg}"
-            echo "usage: $0 [-s|--symbolic]"
-            exit 1
-            ;;
-    esac
-done
-[ -z "${USE_SYMLINKS}" ] || echo "Using symbolic links"
+plugvim="${HOME}/.local/share/nvim/site/autoload/plug.vim" 
+if [ ! -f "${plugvim}" ]; then
+	curl -fLo "${plugvim}" --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	nvim --headless +PlugInstall +qa
+fi
 
-# Install
-mkdir -p "${TARGET}/.config/zsh"
-mkdir -p "${TARGET}/.config/nvim"
-mkdir -p "${TARGET}/.cache/zsh"
-
-FILES=$(find . -path ./.git -prune -o -path ./README.md -prune -o -path ./install.sh -prune -o -type f -print | sed -E 's/^\.\///')
-for f in ${FILES}; do
-    echo "Installing ${TARGET}/${f}"
-    if [ -z "${USE_SYMLINKS}" ]; then
-		rm -f "${TARGET}/${f}"
-        cp "${PWD}/${f}" "${TARGET}/${f}"
-    else
-        ln -sf "${PWD}/${f}" "${TARGET}/${f}"
-    fi
-done
+[[ "${SHELL}" == /bin/zsh ]] || chsh -s /bin/zsh
