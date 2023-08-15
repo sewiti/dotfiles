32f0c51345110b31bb0ce6e50a2a41266babc1bf
Mindaugas Bernotas <mindaugas@bernotas.lt>
2023-08-15 13:09:22 +0300

    Update install script to ask whether to install packages and use sudo

--- install.sh
+++ install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -xe
 
-if   [[ "${OSTYPE}" == darwin* ]]; then echo "TODO"
-elif command -v apt    >/dev/null; then apt update && apt install -y        curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting passwd
-elif command -v dnf    >/dev/null; then dnf install -y                      curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting util-linux-user
-elif command -v zypper >/dev/null; then zypper refresh && zypper install -y curl git neovim wget zsh shadow 
-elif command -v pacman >/dev/null; then pacman -Sy --noconfirm              curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting util-linux zsh-completions
+if [[ "${OSTYPE}" != darwin* ]] && [ -z "${INSTALL_PKGS}" ]; then
+	read -p "Would you like to install packages? (y/n)" INSTALL_PKGS
+fi
+if [[ "${INSTALL_PKGS}" =~ [yY] ]]; then
+	if [ -z "${SUDO_PREFIX}" ] && [ "${EUID}" -ne 0 ]; then
+		SUDO_PREFIX=sudo
+	fi
+
+	if [[ "${OSTYPE}" == darwin* ]]; then
+		echo "TODO"
+	elif command -v apt >/dev/null; then
+		${SUDO_PREFIX} apt update && ${SUDO_PREFIX} apt install -y curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting passwd
+	elif command -v dnf >/dev/null; then
+		${SUDO_PREFIX} dnf install -y curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting util-linux-user
+	elif command -v zypper >/dev/null; then
+		${SUDO_PREFIX} zypper refresh && ${SUDO_PREFIX} zypper install -y curl git neovim wget zsh shadow 
+	elif command -v pacman >/dev/null; then
+		${SUDO_PREFIX} pacman -Sy --noconfirm curl git neovim wget zsh zsh-autosuggestions zsh-syntax-highlighting util-linux zsh-completions
+	fi
 fi
 
 srcdir="$(dirname "$0")"
-cp -f  "${srcdir}/.zshenv" "${HOME}/.zshenv"
-cp -fa "${srcdir}/.config" "${HOME}/.config"
+cp -f  "${srcdir}/.zshenv" "${HOME}/"
+cp -fa "${srcdir}/.config" "${HOME}/"
 mkdir -p "${HOME}/.cache/zsh"
 
 plugvim="${HOME}/.local/share/nvim/site/autoload/plug.vim" 
@@ -19,4 +33,4 @@ if [ ! -f "${plugvim}" ]; then
 	nvim --headless +PlugInstall +qa
 fi
 
-[[ "${SHELL}" == /bin/zsh ]] || chsh -s /bin/zsh
+[ "${SHELL}" = /bin/zsh ] || chsh -s /bin/zsh
